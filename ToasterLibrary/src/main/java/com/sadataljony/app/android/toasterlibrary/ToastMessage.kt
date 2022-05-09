f86496debbddc8c24f752d2ffd925234a34a0ea6
sadataljony <sadataljony@.com>
2022-05-09 12:31:10 +0600

    modify class

--- ToasterLibrary/src/main/java/com/sadataljony/app/android/toasterlibrary/ToastMessage.kt
+++ ToasterLibrary/src/main/java/com/sadataljony/app/android/toasterlibrary/ToastMessage.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 
-object ToasterMessage {
+object ToastMessage {
     fun showToast(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
